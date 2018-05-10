3c03769598700245b17238a3faa0f39770b4ddbf
Allan Burns <allanlburns@yahoo.com>
2018-05-10 01:45:37 +0000

    Add functionality for editing and deleting posts, create Post model.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   end
   
   def show_post
-    post = connection.execute("SELECT * FROM posts WHERE posts.id = ? LIMIT 1", params['id']).first
+    post = Post.find(params['id'])
 
     render 'application/show_post', locals: { post: post }
   end
@@ -16,19 +16,37 @@ class ApplicationController < ActionController::Base
   end
   
   def create_post
-    insert_query = <<-SQL
-      INSERT INTO posts (title, body, author, created_at)
-      VALUES (?, ?, ?, ?)
-    SQL
-
-    connection.execute insert_query,
-      params['title'],
-      params['body'],
-      params['author'],
-      Date.current.to_s
-
+    post = Post.new('title' => params['title'],
+                    'body' => params['body'],
+                    'author' => params['author'])
+    post.save
     redirect_to '/list_posts'
     #binding.pry
+  end
+  
+  def edit_post
+    post = Post.find(params['id'])
+
+    render 'application/edit_post', locals: { post: post }
+  end
+  
+  def update_post
+    update_query = <<-SQL
+      UPDATE posts
+      SET title      = ?,
+          body       = ?,
+          author     = ?
+      WHERE posts.id = ?
+    SQL
+    connection.execute update_query, params['title'], params['body'], params['author'], params['id']
+
+    redirect_to '/list_posts'
+  end
+  
+  def delete_post
+    connection.execute("DELETE FROM posts WHERE posts.id = ?", params['id'])
+
+    redirect_to '/list_posts'
   end
   
   private
---
+++ app/models/post.rb
@@ -0,0 +1,39 @@
+class Post
+  attr_reader :id, :title, :body, :author, :created_at
+
+  def initialize(attributes={})
+    @id = attributes['id']
+    @title = attributes['title']
+    @body = attributes['body']
+    @author = attributes['author']
+    @created_at = attributes['created_at']
+  end
+  
+  def save
+    insert_query = <<-SQL
+      INSERT INTO posts (title, body, author, created_at)
+      VALUES (?, ?, ?, ?)
+    SQL
+
+    connection.execute insert_query,
+      title,
+      body,
+      author,
+      Date.current.to_s
+  end
+  
+  def self.find(id)
+    post_hash = connection.execute("SELECT * FROM posts WHERE posts.id = ? LIMIT 1", id).first
+    Post.new(post_hash)
+  end
+  
+  def self.connection
+    db_connection = SQLite3::Database.new 'db/development.sqlite3'
+    db_connection.results_as_hash = true
+    db_connection
+  end
+
+  def connection
+    self.class.connection
+  end
+end
